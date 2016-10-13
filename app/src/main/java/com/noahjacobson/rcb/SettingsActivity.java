df6b8f51c60764738b0d0adfc52e00a59d19be9c
Noah Jacobson <noahajac@gmail.com>
2016-10-12 23:45:22 -0400

    Removed unused imports.

--- app/src/main/java/com/noahjacobson/rcb/SettingsActivity.java
+++ app/src/main/java/com/noahjacobson/rcb/SettingsActivity.java
@@ -1,6 +1,5 @@
 package com.noahjacobson.rcb;
 
-
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
@@ -13,7 +12,6 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
